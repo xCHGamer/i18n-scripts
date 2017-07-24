f1a2be726a261f75ab4497830a0b9c93a569b6d6
William Venner <william@venner.io>
2017-07-25 00:25:24 +0100

    Update spanish.lua

--- bwhitelist/spanish.lua
+++ bwhitelist/spanish.lua
@@ -1,16 +1,10 @@
 --[[
 
+	BWhitelist Spanish
 
+	Contributors:
 
-BWhitelist English
-
-
-
-Do not make changes to this file, it's used internally
-
-and is not loaded from GitHub.
-
-
+	elolloto
 
 ]]
 
@@ -18,148 +12,148 @@ and is not loaded from GitHub.
 
 {
 
-permission_failure = "Actualmente no tienes permiso para hacer esto.",
+	permission_failure = "Actualmente no tienes permiso para hacer esto.",
 
-disabled_whitelist = "La whitelist para este trabajo esta desactivada.",
+	disabled_whitelist = "La whitelist para este trabajo esta desactivada.",
 
-no_whitelist_permission = "No tienes permiso para editar esta whitelist.",
+	no_whitelist_permission = "No tienes permiso para editar esta whitelist.",
 
-search_jobs = "Buscando trabajos...",
+	search_jobs = "Buscando trabajos...",
 
-steamid_finder = "Buscador de SteamID",
+	steamid_finder = "Buscador de SteamID",
 
-type = "Tipo",
+	type = "Tipo",
 
-value = "Valor",
+	value = "Valor",
 
-team_name = "Nombre",
+	team_name = "Nombre",
 
-select_job = "Selecciona un trabajo a la izquierda...",
+	select_job = "Selecciona un trabajo a la izquierda...",
 
-enable_whitelist = "Activa la Whitelist?",
+	enable_whitelist = "Activa la Whitelist?",
 
-enabling_whitelist = "Activando la Whitelist...",
+	enabling_whitelist = "Activando la Whitelist...",
 
-add_to_whitelist = "Añadir a la Whitelist",
+	add_to_whitelist = "Añadir a la Whitelist",
 
-disable_whitelist = "Desactivar Whitelist",
+	disable_whitelist = "Desactivar Whitelist",
 
-clear_whitelist = "Vaciar Whitelist",
+	clear_whitelist = "Vaciar Whitelist",
 
-search = "Buscar...",
+	search = "Buscar...",
 
-no_data = "No hay datos!",
+	no_data = "No hay datos!",
 
-retrieving = "Recolectando información...",
+	retrieving = "Recolectando información...",
 
-admin_mode = "Modo de Administrador",
+	admin_mode = "Modo de Administrador",
 
-admin_tab = "Administrador",
+	admin_tab = "Administrador",
 
-jobs_tab = "Trabajos",
+	jobs_tab = "Trabajos",
 
-players_tab = "Jugadores",
+	players_tab = "Jugadores",
 
-default_team = "Este es el trabajo por defecto. No puedes whitelist el trabajo por defecto.",
+	default_team = "Este es el trabajo por defecto. No puedes whitelist el trabajo por defecto.",
 
-remove_from_whitelist = "Eliminar de la whitelist",
+	remove_from_whitelist = "Eliminar de la whitelist",
 
-player_name = "Nombre",
+	player_name = "Nombre",
 
-remove_from_whitelist_selected_tooltip = "Elimina el siguiente usuario de una whitelist",
+	remove_from_whitelist_selected_tooltip = "Elimina el siguiente usuario de una whitelist",
 
-add_to_whitelist_selected_tooltip = "Agrega el siguiente usuario a una whitelist",
+	add_to_whitelist_selected_tooltip = "Agrega el siguiente usuario a una whitelist",
 
-clear_whitelist_tooltip = "[Solo Administradores] Vacía la whitelist. Todas las SteamIDs y grupos de usuarios serán borrados.",
+	clear_whitelist_tooltip = "[Solo Administradores] Vacía la whitelist. Todas las SteamIDs y grupos de usuarios serán borrados.",
 
-disable_whitelist_tooltip = "Desactiva la whitelist. Los jugadores podrán unirse al trabajo libremente.",
+	disable_whitelist_tooltip = "Desactiva la whitelist. Los jugadores podrán unirse al trabajo libremente.",
 
-add_to_whitelist_tooltip = "Añade una SteamID, SteamID64 o grupo de usuarios a la whitelist para este trabajo.",
+	add_to_whitelist_tooltip = "Añade una SteamID, SteamID64 o grupo de usuarios a la whitelist para este trabajo.",
 
-remove_from_whitelist_tooltip = "Borra la SteamID, SteamID64 o grupo de usuarios seleccionado/a de la whitelist para este trabajo.",
+	remove_from_whitelist_tooltip = "Borra la SteamID, SteamID64 o grupo de usuarios seleccionado/a de la whitelist para este trabajo.",
 
-in_jobs_file_tooltip = "No puedes desactivar esta whitelist porque BWhitelist está activado en tu archivo de los trabajos de DarkRP.",
+	in_jobs_file_tooltip = "No puedes desactivar esta whitelist porque BWhitelist está activado en tu archivo de los trabajos de DarkRP.",
 
-add_steamid = "Añade una SteamID/64",
+	add_steamid = "Añade una SteamID/64",
 
-add_usergroup = "Añade un grupo de usuarios",
+	add_usergroup = "Añade un grupo de usuarios",
 
-add_usergroup_modal = "Escribe el grupo de usuarios que quieres añadir abajo.",
+	add_usergroup_modal = "Escribe el grupo de usuarios que quieres añadir abajo.",
 
-add_steamid_modal = "Pega la SteamID/SteamID64 que quieras añadir abajo.\nPor favor recuerda que hay una forma más sencilla de hacer esto, ves a la pestaña\"Player\" en el menú!",
+	add_steamid_modal = "Pega la SteamID/SteamID64 que quieras añadir abajo.\nPor favor recuerda que hay una forma más sencilla de hacer esto, ves a la pestaña\"Player\" en el menú!",
 
-error = "Error",
+	error = "Error",
 
-not_a_steamid_or_64 = "Eso no es una SteamID o SteamID64!",
+	not_a_steamid_or_64 = "Eso no es una SteamID o SteamID64!",
 
-already_exists = "Eso ya existe en la whitelist!",
+	already_exists = "Eso ya existe en la whitelist!",
 
-doesnt_exist = "Eso no existe en la whitelist!",
+	doesnt_exist = "Eso no existe en la whitelist!",
 
-copy = "Copiar",
+	copy = "Copiar",
 
-copy_tooltip = "Copia el grupo de usuarios seleccionado o la SteamID.",
+	copy_tooltip = "Copia el grupo de usuarios seleccionado o la SteamID.",
 
-player_left = "Lo siento, pero parece que el usuario se ha desconectado del servidor.",
+	player_left = "Lo siento, pero parece que el usuario se ha desconectado del servidor.",
 
-no_enabled_whitelists = "No hay whitelists activadas a las que tengas acceso!",
+	no_enabled_whitelists = "No hay whitelists activadas a las que tengas acceso!",
 
-import_from_mayoz = "Importa del addon Mayoz's Whitelisting System",
+	import_from_mayoz = "Importa del addon Mayoz's Whitelisting System",
 
-import_from_nordahl = "Importa del addon Nordahl's Whitelisting System",
+	import_from_nordahl = "Importa del addon Nordahl's Whitelisting System",
 
-import_from_old_bwhitelist = "Importa del viejo BWhitelist",
+	import_from_old_bwhitelist = "Importa del viejo BWhitelist",
 
-view = "Mirar",
+	view = "Mirar",
 
-no_import = "Parece que no hemos podido encontrar los archivos/los archivos están corruptos que son requeridos para la importación.",
+	no_import = "Parece que no hemos podido encontrar los archivos/los archivos están corruptos que son requeridos para la importación.",
 
-import_from_old_bwhitelist_tooltip = "Si encontramos algún archivo del viejo BWhitelist en tu servidor, lo convertiremos en los nuevos archivos.",
+	import_from_old_bwhitelist_tooltip = "Si encontramos algún archivo del viejo BWhitelist en tu servidor, lo convertiremos en los nuevos archivos.",
 
-import_from_mayoz_tooltip = "Si encontramos algún archivo del addon Mayoz's Whitelisting System's en tu servidor, lo convertiremos en archivos de BWhitelist .",
+	import_from_mayoz_tooltip = "Si encontramos algún archivo del addon Mayoz's Whitelisting System's en tu servidor, lo convertiremos en archivos de BWhitelist .",
 
-import_from_nordahl_tooltip = "Si encontramos algún archivo del addon Nordahl's Whitelisting System's en tu servidor, lo convertiremos en archivos de BWhitelist.",
+	import_from_nordahl_tooltip = "Si encontramos algún archivo del addon Nordahl's Whitelisting System's en tu servidor, lo convertiremos en archivos de BWhitelist.",
 
-disable_all_whitelists = "Todos los trabajos tendrán su whitelists desactivada. Todos los datos permanecerán intactos.",
+	disable_all_whitelists = "Todos los trabajos tendrán su whitelists desactivada. Todos los datos permanecerán intactos.",
 
-enable_all_whitelists = "Todos los trabajos tendrán sus whitelists activadas.",
+	enable_all_whitelists = "Todos los trabajos tendrán sus whitelists activadas.",
 
-steamprofile = "Perfil de Steam",
+	steamprofile = "Perfil de Steam",
 
-confirm = "Confirmar",
+	confirm = "Confirmar",
 
-are_you_sure = "Seguro que quieres hacer esto?",
+	are_you_sure = "Seguro que quieres hacer esto?",
 
-yes = "Si",
+	yes = "Si",
 
-cancel = "Cancelar",
+	cancel = "Cancelar",
 
-reset_everything = "Resetear todo",
+	reset_everything = "Resetear todo",
 
-reset_everything_tooltip = "ATENCIÓN: Esto borrará ABSOLUTAMENTE TODOS los datos de la whitelist y las whitelists activadas.",
+	reset_everything_tooltip = "ATENCIÓN: Esto borrará ABSOLUTAMENTE TODOS los datos de la whitelist y las whitelists activadas.",
 
-go = "Ir",
+	go = "Ir",
 
-customcheck_team = "Este trabajo tiene un customCheck en el. Esto solapa BWhitelist.\n\nPor favor cambie \"customCheck\" a \"BWhitelist_customCheck\"\nen el archivo de los trabajos para que tu customCheck funcione con BWhitelist.",
+	customcheck_team = "Este trabajo tiene un customCheck en el. Esto solapa BWhitelist.\n\nPor favor cambie \"customCheck\" a \"BWhitelist_customCheck\"\nen el archivo de los trabajos para que tu customCheck funcione con BWhitelist.",
 
-removed_from_whitelist = "Has sido eliminado de la whitelist para \"%s\"",
+	removed_from_whitelist = "Has sido eliminado de la whitelist para \"%s\"",
 
-added_to_whitelist = "Has sido añadido a la whitelist para \"%s\"",
+	added_to_whitelist = "Has sido añadido a la whitelist para \"%s\"",
 
-disable_all_whitelists = "Desactiva todas las whitelists",
+	disable_all_whitelists = "Desactiva todas las whitelists",
 
-enable_all_whitelists = "Activa todas las whitelists",
+	enable_all_whitelists = "Activa todas las whitelists",
 
-player_left = "El jugador se ha desconectado.",
+	player_left = "El jugador se ha desconectado.",
 
-no_actions_available = "No hay acciones disponibles para este jugador.",
+	no_actions_available = "No hay acciones disponibles para este jugador.",
 
-remove_from_all_whitelists = "Elimina de todas las whitelists",
+	remove_from_all_whitelists = "Elimina de todas las whitelists",
 
-add_to_all_whitelists = "Añade a todas las whitelists",
+	add_to_all_whitelists = "Añade a todas las whitelists",
 
-clear_unknown_jobs = "Vaciar trabajos desconocidos",
+	clear_unknown_jobs = "Vaciar trabajos desconocidos",
 
-clear_unknown_jobs_tooltip = "Si hay datos de la whitelist en la base de datos relacionada a un trabajo que ya no existe, todos estos datos serán eliminados con este botón. Los datos de la whitelist de ese trabajo permanecerán en caso de que los necesites en un futuro.",
+	clear_unknown_jobs_tooltip = "Si hay datos de la whitelist en la base de datos relacionada a un trabajo que ya no existe, todos estos datos serán eliminados con este botón. Los datos de la whitelist de ese trabajo permanecerán en caso de que los necesites en un futuro.",
 
 }
