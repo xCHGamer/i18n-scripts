0d106bde41cd4fc80bdbd4a3b6dd082ddf406b9c
William Venner <william@venner.io>
2017-08-27 21:14:13 +0100

    Create norwegian.lua

---
+++ blogs/norwegian.lua
@@ -0,0 +1,121 @@
+--[[
+
+	bLogs Norweigan
+	
+	Contributors:
+	
+	Zaype - STEAM_0:0:63031054
+
+]]
+
+bLogs.Language = {
+	permission_failure = "Du har ikke tilatelse til å gjøre dette.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Admin Modus",
+
+	-- The button to view all logs stored.
+	all_logs = "Alle Logger",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Alle Spillere",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Spillere",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Henter data, vent litt...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Ingen Data!",
+
+	advanced_search = "Avansert Søk",
+	quick_search = "Raskt Søk",
+	search = "Søk",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "SteamID Finner",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Velg noe",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Hopp",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Hopp til side...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Skriv et side nummer (Det er %d sider)",
+
+	cancel = "Cancel",
+
+	error_over_pagenum = "Den siden eksisterer ikke",
+	error_not_num = "Det var ikke et nummer",
+
+	error = "Feil",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Høyre klikk for valg",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Vis spiller logger",
+
+	-- Copy [something] [to clipboard]
+	copy = "Kopier %s",
+
+	profile = "Steam Profil",
+
+	left_click_to_copy = "Venstre klikk for å kopiere",
+
+	modules = "Moduler",
+
+	custom = "Tilpasset",
+
+	enter_a_steamid = "Skriv inn en SteamID...",
+
+	error_not_steamid = "Det var ikke en SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "Og Inneholder...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Farge Modus",
+
+	jump_to_date = "Hopp til dato",
+
+	jump_to_date_tooltip = "Høyre klikk for å hoppe til en dato/tid",
+
+	type_date = "Skriv inn en dato. Den må være i dette formatet: DD/MM/YYYY (%s/%s/%s)",
+
+	error_not_date = "Det var ikke en gyldig dato. Den må være i dette formatet: DD/MM/YYYY (%s/%s/%s)",
+
+	type_time = "Skriv inn ett tidspunkt. Det må være i dette formatet: HH:MM (%s:%s) (24 timer)\nLa være blankt for å ignorere tid.",
+
+	error_not_time = "Det var ikke ett gyldig tidspunkt. Det må være i dette formatet: HH:MM (%s:%s) (24 timer)",
+
+	archive = "Arkiv",
+
+	searching = "Søker",
+
+	search_archive = "Søk i arkivet",
+
+	search_archive_warning = "Å søke i arkivet kan lage hakking på server vis du bruker lokale databaser. Bruk bare hvis det er nødevendig.",
+
+	contribute = "Bidra",
+
+	logging = "Logging",
+
+	save = "Lagre",
+
+	enabled = "Aktivert",
+
+	disabled = "Deaktivert",
+
+	disable = "Deaktiver",
+
+	enable = "Aktiver",
+}
