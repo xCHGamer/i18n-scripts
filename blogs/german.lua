2c11fb341aa7d6547627558ea43cf208d3afea08
William Venner <william@venner.io>
2017-09-02 12:57:52 +0100

    Create german.lua

---
+++ blogs/german.lua
@@ -0,0 +1,159 @@
+--[[
+
+	bLogs German
+
+	Contributors:
+
+	✪๖EXOTIC™ - STEAM_0:1:167507481
+
+]]
+
+bLogs.Language = {
+
+	-- First column in the logs, shows the date/time of the log
+	when = "Wann",
+
+	permission_failure = "Du hast keine Berechtigung dazu.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Admin Modus",
+
+	-- The button to view all logs stored.
+	all_logs = "Alle Logs",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Alle Spieler",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Spieler",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Daten werden abgerufen, bitte warten...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Keine Daten!",
+
+	advanced_search = "Erweitere Suche",
+
+	quick_search = "Schnelle Suche",
+
+	search = "Suche",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "SteamID Finder",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Wähle etwas aus",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Springe",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Springe zur Seite...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Gebe eine Seitennummer ein (es gibt %d Seiten)",
+
+	cancel = "Abbrechen",
+
+	error_over_pagenum = "Diese Seite der Protokolle existiert nicht",
+
+	error_not_num = "Das war keine Nummer",
+
+	error = "Error",
+	-- Left click to copy [player] name
+
+	involved_tooltip = "Rechtsklick für Optionen",
+	-- View logs that that this player is involved in
+
+	view_player_logs = "Spielerprotokolle ansehen",
+	-- Copy [something] [to clipboard]
+
+	copy = "Kopiert %s",
+
+	profile = "Steam Profil",
+
+	left_click_to_copy = "Linksklick um zum Kopieren",
+
+	modules = "Module",
+
+	custom = "Custom",
+
+	enter_a_steamid = "Gebe eine SteamID rein...",
+
+	error_not_steamid = "Das war nicht eine SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "Und enthält...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Farben Modus",
+
+	jump_to_date = "Springe zum Datum",
+
+	jump_to_date_tooltip = "Rechtsklick, um einem Datum/Uhrzeit zu springen",
+
+	type_date = "Gebe ein Datum ein. Es muss in diesem Format sein: DD/MM/YYYY (%s/%s/%s)",
+
+	error_not_date = "Das war kein gültiges Datum. Es muss in diesem Format sein: DD/MM/YYYY (%s/%s/%s)",
+
+	type_time = "Enter a timestamp. It must be in this format: HH:MM (%s:%s) (24 hour)\nLeave blank to ignore time.",
+
+	error_not_time = "Das war kein gültiger Zeitstempel. Es muss in diesem Format sein: HH:MM (%s:%s) (24 Stunden)",
+
+	archive = "Archiv",
+
+	searching = "Suchen",
+
+	search_archive = "Durchsuche Archiv",
+
+	search_archive_warning = "Die Suche nach dem Archiv kann auf dem Server auf lokalen Datenbanken verzichten. Nur bei Bedarf benutzen.",
+
+	logging = "Protokollierung",
+
+	save = "Speichern",
+
+	enabled = "Aktiviert",
+
+	disabled = "Deaktiviert",
+
+	disable = "Deaktivieren",
+
+	enable = "Aktivieren",
+
+	give = "Geben",
+
+	take = "Nehmen",
+
+	new_steamid = "Neue SteamID",
+
+	new_usergroup = "Neue Usergroup",
+
+	usergroup_name = "Usergroup Name",
+
+	-- "Delete this entry from permissions"
+	delete_entry = "Eintrag löschen",
+
+	usergroup_exists = "That usergroup already exists in the permissions! Is there a CAMI usergroup for that? Use that if so.",
+
+	steamid_exists = "Dieser SteamID existiert bereits in den Berechtigungen.",
+
+	loading = "Lädt...",
+
+	support = "Support",
+
+	-- "Player Format" is a setting that contains everything that will be included
+	-- in a log that references a player. For example, it will show the player's job,
+	-- health, armour, etc. depending on the settings.
+	player_format = "Player Format",
+
+	permissions = "Berechtigungen",
+
+	general_settings = "Allgemeine Einstellungen",
+
+	operations = "Operationen",
+	
+}
