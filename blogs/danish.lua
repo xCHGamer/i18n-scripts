f1617241df8f7249ce3d8f70c3c1d8eeb6392002
Måge <danishhax@hotmail.com>
2017-12-10 01:39:38 +0100

    Create danish.lua
    Danish translation of blogs

---
+++ blogs/danish.lua
@@ -0,0 +1,154 @@
+--[[
+	bLogs Danish
+	
+Contribrutors:
+
+Måge - STEAM_0:1:82373951
+]]
+
+{
+
+	-- First column in the logs, shows the date/time of the log
+	when = "Hvornår",
+
+	permission_failure = "Du har ikke adgang til dette.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Admin Tilstand",
+
+	-- The button to view all logs stored.
+	all_logs = "Alle Logs",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Alle spillere",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Spillere",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Indlæser data, vent venligst...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Ingen data!",
+
+	advanced_search = "Advanceret søg",
+	quick_search = "Hurtig søg",
+	search = "Søg",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "SteamID Finder",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Vælg noget",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Hop",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Hop til side...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Skriv et side nummer (der er %d sider)",
+
+	cancel = "Annuller",
+
+	error_over_pagenum = "Den side af logs findes ikke",
+	error_not_num = "Det var ikke et nummer",
+
+	error = "Fejl",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Højre klik for indstillinger",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Vis spiller logs",
+
+	-- Copy [something] [to clipboard]
+	copy = "Kopier %s",
+
+	profile = "Steam Profil",
+
+	left_click_to_copy = "Venstrr klik for at kopiere",
+
+	modules = "Moduler",
+
+	custom = "Tilpassede",
+
+	enter_a_steamid = "Indtast et SteamID...",
+
+	error_not_steamid = "Det var ikke et SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "Og indeholder...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Farve tilstand",
+
+	jump_to_date = "Hop til dato",
+
+	jump_to_date_tooltip = "Højre klik for at hoppe til en dato/tid",
+
+	type_date = "Indtast en dato. Den skal være i denne format: DD/MM/YYYY (%s/%s/%s)",
+
+	error_not_date = "Det var ikke en rigtig dato. Den skal være denne format: DD/MM/YYYY (%s/%s/%s)",
+
+	type_time = "Indtast et tidspunkt. Det skal være i denne format: HH:MM (%s:%s) (24 hour)\nBehold blank for at ignorere tid.",
+
+	error_not_time = "Det var ikke et rigtigt tidspunkt. Det skal være i denne format: HH:MM (%s:%s) (24 hour)",
+
+	archive = "Arkiv",
+
+	searching = "Søger",
+
+	search_archive = "Søg arkiv",
+
+	search_archive_warning = "Søgning af arkivet kan lagge serveren på lokale databaser. Brug kun hvis nødvendigt.",
+
+	logging = "Logging",
+
+	save = "Gem",
+
+	enabled = "Aktiveret",
+
+	disabled = "Deaktiveret",
+
+	disable = "Deaktiver",
+
+	enable = "Aktiver",
+
+	give = "Giv",
+
+	take = "Tag",
+
+	new_steamid = "Nyt SteamID",
+
+	new_usergroup = "Ny Brugergruppe",
+
+	usergroup_name = "Brugergruppe Navn",
+
+	-- "Delete this entry from permissions"
+	delete_entry = "Slet notat",
+
+	usergroup_exists = "Den brugergruppe eksisterer allerede i tilladelserne! Er der en CAMI brugergruppe til det? Brug den hvis ja.",
+
+	steamid_exists = "Det SteamID findes allerede i tilladelserne.",
+
+	loading = "Indlæser...",
+
+	support = "Support",
+
+	-- "Player Format" is a setting that contains everything that will be included
+	-- in a log that references a player. For example, it will show the player's job,
+	-- health, armour, etc. depending on the settings.
+	player_format = "Spiller Format",
+
+	permissions = "Tilladelser",
+
+	general_settings = "Generelle indstillinger",
+
+	operations = "Oprationer",
+
+}
