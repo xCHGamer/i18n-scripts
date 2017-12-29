0ab90cb7ed827ac892d98b8cce4520447f437a74
Davide Josè Paci <djosepaci@gmail.com>
2017-12-30 00:37:31 +0100

    Create italian.lua
    Here you go!

---
+++ blogs/italian.lua
@@ -0,0 +1,158 @@
+
+--[[
+
+	bLogs Italian
+  
+	Contributors:
+	
+  Postino - STEAM_0:0:48683683
+  FeFoX - STEAM_0:1:29136953
+  
+]]
+
+{
+
+	-- First column in the logs, shows the date/time of the log
+	when = "Quando",
+
+	permission_failure = "Non hai i permessi per fare questa azione.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Modalità Admin",
+
+	-- The button to view all logs stored.
+	all_logs = "Tutti i Log",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Tutti i Giocatori",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Giocatori",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Ricevendo i dati, perfavore aspetta...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Nessun dato!",
+
+	advanced_search = "Ricerca Avanzata",
+	quick_search = "Ricerca Veloce",
+	search = "Ricerca",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "SteamID Finder",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Seleziona qualcosa",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Salta",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Salta a pagina...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Inserisci il numero di una pagina (ci sono %d pagine)",
+
+	cancel = "Annulla",
+
+	error_over_pagenum = "Quella pagina non esiste",
+	error_not_num = "Quello non era un numero",
+
+	error = "Errore",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Tasto destro per opzioni",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Visualizza Log Giocatore",
+
+	-- Copy [something] [to clipboard]
+	copy = "Copia %s",
+
+	profile = "Profilo Steam",
+
+	left_click_to_copy = "Tasto sinistro per copiare",
+
+	modules = "Moduli",
+
+	custom = "Personalizzato",
+
+	enter_a_steamid = "Inserisci uno SteamID...",
+
+	error_not_steamid = "Quello non era uno SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "E contiene...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Modalità Colore",
+
+	jump_to_date = "Salta a una Data",
+
+	jump_to_date_tooltip = "Click destro per saltare a una data/ora",
+
+	type_date = "Inserisci una data. Dev'essere in questo formato: GG/MM/AAAA (%s/%s/%s)",
+
+	error_not_date = "La data inserita non è valida. Dev'essere in questo formato: GG/MM/AAAA (%s/%s/%s)",
+
+	type_time = "Inserisci un orario. Dev'essere inserito in questo formato HH:MM (%s:%s) (24 Ore)\nLascia in bianco se vuoi ignorare l'ora.",
+
+	error_not_time = "L'orario inserito non è valido. Dev'essere inserito in questo formato HH:MM (%s:%s) (24 Ore)",
+
+	archive = "Archivio",
+
+	searching = "Cercando",
+
+	search_archive = "Ricerca Archivio",
+
+	search_archive_warning = "Fare una ricerca in archivio può far laggare il server se si sta utilizzando il databse locale. Utilizzare solo se necessario.",
+
+	logging = "Loggando",
+
+	save = "Salva",
+
+	enabled = "Abilitato",
+
+	disabled = "Disabilitato",
+
+	disable = "Disabilita",
+
+	enable = "Abilità",
+
+	give = "Dai",
+
+	take = "Prendi",
+
+	new_steamid = "Nuovo SteamID",
+
+	new_usergroup = "Nuovo Gruppo Utente",
+
+	usergroup_name = "Nome Gruppo Utente",
+
+	-- "Delete this entry from permissions"
+	delete_entry = "Cancella Inserzione",
+
+	usergroup_exists = "Quel gruppo è già inserito nei permessi! Esiste per caso un gruppo CAMI per quello? Se sì, usa quello.",
+
+	steamid_exists = "Quello SteamID risulta già incluso nei permessi.",
+
+	loading = "Caricamento...",
+
+	support = "Supporto",
+
+	-- "Player Format" is a setting that contains everything that will be included
+	-- in a log that references a player. For example, it will show the player's job,
+	-- health, armour, etc. depending on the settings.
+	player_format = "Informazioni Giocatore",
+
+	permissions = "Permessi",
+
+	general_settings = "Impostazioni Generali",
+
+	operations = "Operazioni",
+
+}
