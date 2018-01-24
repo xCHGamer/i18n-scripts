e34d5ee6ae29c1a9e58f99844576f6062e1a8cd1
William Venner <william@venner.io>
2018-01-24 23:19:37 +0000

    Update turkish.lua

--- blogs/turkish.lua
+++ blogs/turkish.lua
@@ -1,64 +1,36 @@
-
+--[[
 
 	bLogs Turkish
 
-	Bu Bilgileri Değiştirmeyin!
-	(heriporter22) Translated By
+	Contributors:
 
+	heriporter22
 
 ]]
 
-
-
-{
-
-
-
+bLogs.Language = {
 	-- First column in the logs, shows the date/time of the log
-
 	when = "Sonra",
-
-
 
 	permission_failure = "Bunu Yapmak İçin Yeterli İzinlere Sahip Değilsin!",
 
-
-
 	-- When the user is an admin, this will be in the window title.
-
 	admin_mode = "Yetkili Modu",
 
-
-
 	-- The button to view all logs stored.
-
 	all_logs = "Tüm Loglar",
 
-
-
 	-- The button to view all players stored in bLogs.
-
 	all_players = "Tüm Oyuncular",
 
-
-
 	-- The tab at the top to view players stored in bLogs.
-
 	players = "Oyuncular",
 
-
-
 	-- The message shown to the player when they first open bLogs.
-
 	getting_data = "Bilgiler Alınıyor. Lütfen Bekleyin...",
 
-
-
 	-- The message shown when there's no logs to show.
-
 	no_data = "Bilgi Yok!",
-
-
 
 	advanced_search = "Gelişmiş Arama",
 
@@ -66,234 +38,120 @@
 
 	search = "Arama",
 
-
-
 	-- The button to open the SteamID Finder (steamid.venner.io)
-
 	steamid_finder = "SteamID Bulucu",
 
-
-
 	-- The message in the players panel instructing the user to select something
-
 	-- to view players.
-
 	select_something = "Birşey Seç",
 
-
-
 	-- The button at the pagination that allows you to jump to a specific page.
-
 	jump = "Geç",
 
-
-
 	-- The title of the window that pops up when you click "Jump"
-
 	jump_to_page = "Diğer Sayfaya Geç...",
 
-
-
 	-- The text in that window instructing the user.
-
 	-- %d is the number of pages that exist
-
 	type_page = "Sayfa Numarası Yaz (%d Sayfa Var.)",
 
-
-
 	cancel = "İptal",
-
-
 
 	error_over_pagenum = "Bu Sayfada Loglar Bulunamadı!",
 
 	error_not_num = "Bu Bir Numara Değil!",
 
-
-
 	error = "Hata",
 
-
-
 	-- Left click to copy [player] name
-
 	involved_tooltip = "Ayarları Görmek İçin Sağ Tıkla",
 
-
-
 	-- View logs that that this player is involved in
-
 	view_player_logs = "Oyuncu Loglarını Görüntüle",
 
-
-
 	-- Copy [something] [to clipboard]
-
 	copy = "Kopyala %s",
-
-
 
 	profile = "Steam Profili",
 
-
-
 	left_click_to_copy = "Kopyalamak İçin Sol İle Tıkla",
-
-
 
 	modules = "Modüller",
 
-
-
 	custom = "Farklı",
-
-
 
 	enter_a_steamid = "SteamID Gir...",
 
-
-
 	error_not_steamid = "Bu Bir SteamID Değil!",
 
-
-
 	-- Used in the advanced search box
-
 	-- "Does the log contain this text?"
-
 	contains = "Ve İçindekiler...",
 
-
-
 	-- "Colour Mode" is whether or not the logs are shown with coloured text.
-
 	colour_mode = "Renk Modu",
-
-
 
 	jump_to_date = "Tarihe Git",
 
-
-
 	jump_to_date_tooltip = "O Zamana Gitmek İçin Sol Tıkla",
-
-
 
 	type_date = "Tarih Gir. Formatı Böyle Olmalıdır: GG/AA/YYYY (%s/%s/%s)",
 
-
-
 	error_not_date = "Girdiğiniz Tarih Doğru Değil. Formatı Böyle Olmalıdır: GG/AA/YYYY (%s/%s/%s)",
-
-
 
 	type_time = "Saati Girin. Formatı Böyle Olmalıdır: SS:DD (%s:%s) (24 hour)\nSaati Girmek İstemiyorsanız Boşluk Bırakın.",
 
-
-
 	error_not_time = "Doğru Saat Değil. Formatı Böyle Olmalıdır: SS:DD (%s:%s) (24 hour)",
-
-
 
 	archive = "Arşiv",
 
-
-
 	searching = "Aranıyor...",
-
-
 
 	search_archive = "Arşiv Ara",
 
-
-
 	search_archive_warning = "Arşivde Birşey Ararken Sunucuda Lag Olmaktadır. Lütfen Sadece İhtiyacınız Olanları Arayın.",
-
-
 
 	logging = "Logging",
 
-
-
 	save = "Kaydet",
-
-
 
 	enabled = "Açık",
 
-
-
 	disabled = "Kapalı",
-
-
 
 	disable = "Kapat",
 
-
-
 	enable = "Aç",
-
-
 
 	give = "Ver",
 
-
-
 	take = "Al",
-
-
 
 	new_steamid = "Yeni SteamID",
 
-
-
 	new_usergroup = "Yeni Kullanıcı Grubu",
-
-
 
 	usergroup_name = "Kullanıcı Grubu Adı",
 
-
-
 	-- "Delete this entry from permissions"
-
 	delete_entry = "Girdiyi Sil",
-
-
 
 	usergroup_exists = "Bu Kullanıcı Grubu Zaten Bu İzinlere Sahip.",
 
-
-
 	steamid_exists = "Bu SteamID Zaten Bu İzinlere Sahip.",
-
-
 
 	loading = "Yükleniyor...",
 
-
-
 	support = "Destek",
 
-
-
 	-- "Player Format" is a setting that contains everything that will be included
-
 	-- in a log that references a player. For example, it will show the player's job,
-
 	-- health, armour, etc. depending on the settings.
-
 	player_format = "Oyuncu Formatı",
-
-
 
 	permissions = "İzinler",
 
-
-
 	general_settings = "Genel Ayarlar",
 
-
-
 	operations = "Operation'lar",
+}
