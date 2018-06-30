e88a740e5a3ace6749fd1729885b81d34962fd84
xCHGamer <40723529+xCHGamer@users.noreply.github.com>
2018-06-30 23:00:55 +0300

    Create finnish.lua

---
+++ blogs/finnish.lua
@@ -0,0 +1,154 @@
+--[[
+	bLogs Finnish
+	
+	Contributors:
+	
+	CHGamer - STEAM_1:1:230114213
+]]
+
+{
+
+	-- First column in the logs, shows the date/time of the log
+	when = "Milloin",
+
+	permission_failure = "Sinulla ei ole oikeutta suorittaa toimintoa.",
+
+	-- When the user is an admin, this will be in the window title.
+	admin_mode = "Admin-Tila",
+
+	-- The button to view all logs stored.
+	all_logs = "Kaikki lokit",
+
+	-- The button to view all players stored in bLogs.
+	all_players = "Kaikki Pelaajat",
+
+	-- The tab at the top to view players stored in bLogs.
+	players = "Pelaajat",
+
+	-- The message shown to the player when they first open bLogs.
+	getting_data = "Haetaan dataa, odota hetki...",
+
+	-- The message shown when there's no logs to show.
+	no_data = "Ei dataa!",
+
+	advanced_search = "Tarkennettu Haku",
+	quick_search = "Nopea Haku",
+	search = "Haku",
+
+	-- The button to open the SteamID Finder (steamid.venner.io)
+	steamid_finder = "SteamID-etsin",
+
+	-- The message in the players panel instructing the user to select something
+	-- to view players.
+	select_something = "Valitse jotain",
+
+	-- The button at the pagination that allows you to jump to a specific page.
+	jump = "Hyppää",
+
+	-- The title of the window that pops up when you click "Jump"
+	jump_to_page = "Hyppää sivulle...",
+
+	-- The text in that window instructing the user.
+	-- %d is the number of pages that exist
+	type_page = "Kirjoita sivunumero (yhteensä %d sivua)",
+
+	cancel = "Peruuta",
+
+	error_over_pagenum = "Tätä sivua ei ole olemassa.",
+	error_not_num = "Se ei ollut numero..",
+
+	error = "Virhe",
+
+	-- Left click to copy [player] name
+	involved_tooltip = "Napsauta hiiren oikealla painikkeella nähdäksesi vaihtoehdot",
+
+	-- View logs that that this player is involved in
+	view_player_logs = "Näytä pelaajalokit",
+
+	-- Copy [something] [to clipboard]
+	copy = "Kopioi %s",
+
+	profile = "Steam Profiili",
+
+	left_click_to_copy = "Kopioi hiiren vasemmalla painikkeella.",
+
+	modules = "Moduulit",
+
+	custom = "Mukautettu",
+
+	enter_a_steamid = "Syötä SteamID...",
+
+	error_not_steamid = "Se ei ollut SteamID.",
+
+	-- Used in the advanced search box
+	-- "Does the log contain this text?"
+	contains = "Ja sisältää...",
+
+	-- "Colour Mode" is whether or not the logs are shown with coloured text.
+	colour_mode = "Väritila",
+
+	jump_to_date = "Siirry päivämäärään",
+
+	jump_to_date_tooltip = "Napsauta hiiren oikealla painikkeella siirtyäksesi päivämäärään/aikaan.",
+
+	type_date = "Syötä päivämäärä. Sen täytyy olla seuraavassa muodossa: PV/KK/V (%s/%s/%s)",
+
+	error_not_date = "Tuo ei ollut päivämäärä.. Sen täytyy olla seuraavassa muodossa: PV/KK/V (%s/%s/%s)",
+
+	type_time = "Syötä aika. Sen täytyy olla seuraavassa muodossa: T:MIN (%s:%s) (24h)\nJätä tyhjäksi jättääksesi ajan huomioimatta.",
+
+	error_not_time = "Tuo ei ollut aika. Sen täytyy olla seuraavassa muodossa: T:MIN (%s:%s) (24h)",
+
+	archive = "Arkisto",
+
+	searching = "Etsitään",
+
+	search_archive = "Etsi Arkistosta",
+
+	search_archive_warning = "Arkiston etsiminen voi viivästää palvelinta paikallisissa tietokannoissa. Käytä vain tarvittaessa.",
+
+	logging = "Kirjataan",
+
+	save = "Tallenna",
+
+	enabled = "Käytössä",
+
+	disabled = "Pois käytöstä",
+
+	disable = "Poista käytöstä",
+
+	enable = "Ota käyttöön",
+
+	give = "Anna",
+
+	take = "Ota",
+
+	new_steamid = "Uusi SteamID",
+
+	new_usergroup = "Uusi Käyttäjäryhmä",
+
+	usergroup_name = "Käyttäjäryhmän Nimi",
+
+	-- "Delete this entry from permissions"
+	delete_entry = "Poista Merkintä",
+
+	usergroup_exists = "Tämä käyttäjäryhmä on jo käyttöoikeuksissa! Onko olemassa CAMI-käyttäjäryhmä? Käytä sitä jos niin.",
+
+	steamid_exists = "Tuo SteamID on jo käyttöoikeuksissa.",
+
+	loading = "Ladataan...",
+
+	support = "Tuki",
+
+	-- "Player Format" is a setting that contains everything that will be included
+	-- in a log that references a player. For example, it will show the player's job,
+	-- health, armour, etc. depending on the settings.
+	player_format = "Pelaajamuoto",
+
+	permissions = "Käyttöoikeudet",
+
+	general_settings = "Yleiset Asetukset",
+
+	operations = "Toiminnot",
+
+}
